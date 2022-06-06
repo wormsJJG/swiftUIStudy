09614be96fc89b60a9200ba15cd5fd0077351dd7
wormsJJG <jjr66461837@gmail.com>
2022-06-06 23:06:25 +0900

    Making LoginView

--- SwiftUIStudy/SwiftUIStudy/ContentView.swift
+++ SwiftUIStudy/SwiftUIStudy/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var id: String = ""
+    @State var passWord: String = ""
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack{
+                TextField("아이디", text: $id)
+                    .padding()
+                    .background(Color(uiColor: .orange))
+                    .textFieldStyle(.roundedBorder)
+                    .cornerRadius(10)
+                
+                SecureField("비밀번호", text: $passWord)
+                    .padding()
+                    .background(Color(uiColor: .orange))
+                    .textFieldStyle(.roundedBorder)
+                    .cornerRadius(10)
+                Button("로그인"){
+                    
+                }
+                .background(Color(uiColor: .orange))
+                .buttonStyle(.bordered)
+                .cornerRadius(10)
+            }
             .padding()
+            .background(Color(uiColor: .yellow))
+            
+
+            
+            
+            
+            .navigationTitle("로그인")
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
